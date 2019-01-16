f4cb19a375bda6ce29da68c51a525ad7dda1e8b2
chunbong <cdy0266@gmail.com>
2019-01-16 19:24:05 +0900

    Work to save the user name

--- greeting.js
+++ greeting.js
@@ -1,27 +1,51 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = documnet.querySelector(".js-greeting");
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LOCAL_STORAGE = "currentUser",
     SHOWING_CLASS_NAME = "showing";
 
+
+function saveName(text) {
+    localStorage.setItem(USER_LOCAL_STORAGE, text);
+    console.log(USER_LOCAL_STORAGE);
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+
+function askForName() {
+    form.classList.add(SHOWING_CLASS_NAME);
+    form.addEventListener("submit", handleSubmit);
+}
+
+
 function paintGreeting(text) {
+    form.classList.remove(SHOWING_CLASS_NAME);
     greeting.classList.add(SHOWING_CLASS_NAME);
     greeting.innerText = `Hello ${text}`;
 }
+
 
 function loadName() {
     const currentUser = localStorage.getItem(USER_LOCAL_STORAGE);
 
     if ( currentUser === null ) {
-
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
 }
 
-function init() {
 
+function init() {
+    loadName();
 }
 
 init();
