0f8cc2106ebb5a28cd59d1fc0c93bbacbf79343b
chunbong <cdy0266@gmail.com>
2019-01-17 21:38:31 +0900

    Work to delete the To Do items

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LOCAL_STORAGE = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+
+function deleteToDo(event) {
+   const btn = event.target;
+   const li = btn.parentNode;
+   toDoList.removeChild(li);
+   const cleanToDos = toDos.filter(function(toDo) {
+      return toDo.id !== parseInt(li.id);
+   });
+   toDos = cleanToDos;
+   saveToDos();
+}
 
 
 function saveToDos() {
@@ -18,6 +30,7 @@ function paintToDo(text) {
    const span = document.createElement("span");
    const newId = toDos.length + 1;
    delBtn.innerHTML = "❌";
+   delBtn.addEventListener("click", deleteToDo);
    span.innerHTML = text;
    li.appendChild(delBtn);
    li.appendChild(span);
@@ -43,8 +56,11 @@ function handleSubmit(event) {
 function loadTodos() {
    const loadedToDos = localStorage.getItem(TODOS_LOCAL_STORAGE);
 
+   // localStorage가 TODO 목록을 가지고 있다면
    if ( loadedToDos !== null ) {
+      // JSON 형태의 TODO 목록을 자바스크립트 형태로 변경
       const parsedToDos = JSON.parse(loadedToDos);
+      // 각 항목마다 함수를 실행하는데
       parsedToDos.forEach(function(toDo) {
          paintToDo(toDo.text);
       });
