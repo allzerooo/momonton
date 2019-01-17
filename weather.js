67072cf81f03312c9216646da23cefb9e9fff155
chunbong <cdy0266@gmail.com>
2019-01-18 00:17:08 +0900

    Work on getting weather information by location information

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
 const COORDS = 'coords';
+const API_KEY = 'db2081aece7de6c4ffa2052cf3840ff4';
+
+const weather = document.querySelector(".js-weather");
+
+
+function getWeather(lat, lon) {
+   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+   .then(function(response){
+      return response.json();
+   }).then(function(json){
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @${place}`;
+   });
+}
 
 
 function saveCoords(coordsObj) {
@@ -33,7 +48,8 @@ function loadCoords() {
    if ( loadedCoords === null ) {
       askForCoords();
    } else {
-
+      const parsedCoords = JSON.parse(loadedCoords);
+      getWeather(parsedCoords.latitude, parsedCoords.longitude);
    }
 }
 
